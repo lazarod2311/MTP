3ce2ba7ba719aeb706a3751e181c8f8d93e01fa1
lazarod2311 <lduarteneto@gmail.com>
2017-12-20 22:17:51 -0200

    Trabalho feito!!

--- aula20171207/funcoes.h
+++ aula20171207/funcoes.h
@@ -1,105 +1,290 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
-struct cliente {
+#include<conio.h>
+typedef struct cliente {
 	char nome [200]; 
 	char rg[10]; 
 	char cpf[11];
 	int idade;
 	char usuario [200];
-	char senha [200];
+	char senha [4];
 	float saldo;
-	float *inserir;
-	int *vezes;
+	float inserir[400];
+	int vezes_inserir[400];
+	float retirar[400];
+	int vezes_r[400];
+	int tam_inseriu;
+	int tam_retirou;
 }Cliente;
 int j, l, p;
-void criar_conta(int i) {
-	Cliente c[i];
+void criar_conta() {
+	FILE *arq;
+	Cliente c;
 	printf ("Insira os seguintes dados:\n");
-	printf ("1 - Nome: ");
-	gets(c[i-1].nome);
-	printf ("\n2 - Idade: ");
-	scanf ("%d" ,&c[i-1].idade);
-	printf ("\n3 - RG: ");
-	gets(c[i-1].rg);
-	printf ("\n4 - CPF: ");
-	gets(c[i-1].cpf);
-	printf ("\n5 - Usuario: ");
-	gets(c[i-1].usuario);
-	printf ("\n6 - Senha: ");
-	gets(c[i-1].senha);
-	if (c[i-1].idade < 18) {
+	printf ("\n1 - Nome: ");
+	fgets(c.nome,200,stdin);
+	printf ("2 - Idade: ");
+	scanf ("%d" ,&c.idade);
+	if (c.idade < 18) {
 		printf ("Cadastro rejeitado!!");
-		break;
 	}
-	getch();
+	printf ("3 - RG: ");
+	scanf ("%s" ,&c.rg);
+	printf ("4 - CPF: ");
+	scanf ("%s" ,&c.cpf);
+	printf ("5 - Usuario: ");
+	scanf ("%s" ,&c.usuario);
+	printf ("6 - Senha de quatro digitos: ");
+	scanf ("%s" ,&c.senha);
+	c.saldo = 0;
+	c.tam_inseriu = 0;
+	c.tam_retirou = 0;
+	arq = fopen (c.cpf, "wb");
+	fwrite(&c, sizeof(Cliente), 1, arq);
+	fclose (arq);
 }
-void mostrar_conta() {
-	char Cpf[10];
+void mostrar_conta(char Cpf[]) {
+	char senha[4];
+	Cliente c;
 	int i, k;
-	Cliente *c1 = adquirir_estrut();
-	printf ("Digite seu CPF: ");
-	gets(Cpf);
-	for (i=0; i<j; i++){
-	if (strcmp(c1[i].cpf,Cpf) == 0){
-		k = i;
-	} 
+	FILE *arq;
+	printf ("\nDigite seu CPF: ");
+	scanf ("%s" ,Cpf);
+	arq = fopen(Cpf, "rb");
+	if (arq == NULL) {
+	printf ("\nUsuario nao cadastrado!!\n");
+	return;
 	}
-	printf ("Nome: %s" ,c1[k].nome);
-	printf ("\nIdade: %d" ,c1[k].idade);
-	printf ("\nRG: %s" ,c1[k].rg);
-	printf ("\nCPF: %s" ,c1[k].cpf);
-	getch();
+	fread(&c, sizeof(Cliente), 1, arq);
+	fclose (arq);
+	printf ("\nDigite sua senha: ");
+	senha[0] = getch(); 
+	printf ("*"); 
+	senha[1] = getch(); 
+	printf ("*"); 
+	senha[2] = getch(); 
+	printf ("*"); 
+	senha[3] = getch();  
+	printf ("*\n"); 
+	if((senha[0] == c.senha[0]) && (senha[1] == c.senha[1]) && (senha[2] == c.senha[2]) && (senha[3] == c.senha[3]))  {
+	printf ("\nNome: %s" ,&c.nome);
+	printf ("Idade: %d" ,c.idade);
+	printf ("\nRG: %s" ,c.rg);
+	printf ("\nCPF: %s" ,c.cpf);
+	printf ("\nNome de usuario: %s\n" ,c.usuario);
+	}
+	else 
+	printf ("\nSenha incorreta!!\n");
 }
-void mostrar_saldo() {
-	char Cpf[10];
+void mostrar_saldo(char Cpf[]) {
+	char senha[4];
+	FILE *arq;
+	Cliente c;
 	int i, k;
-	Cliente *c1 = adquirir_estrut();
-	printf ("Digite seu CPF: ");
-	gets(Cpf);
-	for (i=0; i<j; i++){
-	if (strcmp(c1[i].cpf,Cpf) == 0){
-		k = i;
-	} 
+	if (Cpf[0] == '\0') {
+	printf ("\nDigite seu CPF: ");
+	scanf ("%s" ,Cpf);
 	}
-	printf ("Saldo: %f" ,c1[k].saldo);
-	getch();
+	arq = fopen(Cpf, "rb");
+	if (arq == NULL) {
+	printf ("\nUsuario nao cadastrado!!\n");
+	return;
+	}
+	fread(&c, sizeof(Cliente), 1, arq);
+	fclose (arq);
+	printf ("\nDigite sua senha: ");
+	senha[0] = getch(); 
+	printf ("*"); 
+	senha[1] = getch(); 
+	printf ("*"); 
+	senha[2] = getch(); 
+	printf ("*"); 
+	senha[3] = getch();  
+	printf ("*\n"); 
+	if((senha[0] == c.senha[0]) && (senha[1] == c.senha[1]) && (senha[2] == c.senha[2]) && (senha[3] == c.senha[3]))  {
+	printf ("\nSaldo: R$ %g\n\n" ,c.saldo);
+	}
+	else 
+	printf ("\nSenha incorreta!!\n");
 }
-void inserir_dinheiro() {
-	char Cpf[10];
-	int i=0, k, m, v=1, t=1, o;
-	Cliente *c1 = adquirir_estrut();
-	printf ("Digite seu CPF: ");
-	gets(Cpf);
-	for (i=0; i<j; i++){
-	if (strcmp(c1[i].cpf,Cpf) == 0){
-		k = i;
-	} 
+void inserir_dinheiro(char Cpf[]) {
+	char senha[4];
+	float inserir;
+	FILE *arq;
+	int i=0, k, m, v=0, t=1, opt;
+	Cliente c;
+	if (Cpf[0] == '\0') {
+	printf ("\nDigite seu CPF: ");
+	scanf ("%s" ,Cpf);
 	}
-	c1[k].inserir = malloc(1);
-	c1[k].vezes_inserir = malloc(1);
-	printf ("Insira o saldo: ");
+	arq = fopen(Cpf, "rb");
+	if (arq == NULL) {
+	printf ("\nUsuario nao cadastrado!!\n");
+	return;
+	}
+	fread(&c, sizeof(Cliente), 1, arq);
+	fclose (arq);
+	printf ("\nDigite sua senha: ");
+	senha[0] = getch(); 
+	printf ("*"); 
+	senha[1] = getch(); 
+	printf ("*"); 
+	senha[2] = getch(); 
+	printf ("*"); 
+	senha[3] = getch();  
+	printf ("*\n"); 
+	if((senha[0] == c.senha[0]) && (senha[1] == c.senha[1]) && (senha[2] == c.senha[2]) && (senha[3] == c.senha[3]))  {
+	
 	do {
-		v++;
-		scanf ("%f",&c1[k].inserir[i]);
-		c1[k].vezes_inserir[i] = v;
-		printf ("Deseja realizar outra operacao? Digite 1 para sim ou 2 para nao.");
+	printf ("\nInsira o saldo: ");
+	scanf ("%f",&inserir);
+		for (i=0;i<c.tam_inseriu;i++) {
+			if (inserir == c.inserir[i]) {
+			c.vezes_inserir[i] ++;
+			v = 10;
+			}
+		}
+			if (v != 10){
+				k = c.tam_inseriu;
+				c.inserir[k] = inserir;
+				c.tam_inseriu ++;
+				c.vezes_inserir[k]=1;
+				v = 0;
+			}
+		c.saldo = c.saldo + inserir;
+		printf ("Deseja realizar outra operacao? Digite 1 para sim ou 2 para nao: ");
 		scanf ("%d" ,&m);
-		if (m == 1) {
-			printf ("Para realizar operacao de mesmo valor pressione 1, ou 2 para valor diferente: ");
-			scanf ("%d" ,&opt);
-		}
-		if (opt == 1){
-			v++;
-			c1[k].vezes_inserir[i] = v;
-		}
-		if (opt == 2){
-			i++;
-			c1[k].inserir = realloc(c1[k].inserir,(t+1)*sizeof(float));
-			t++;
-			v = 1;
-			scanf ("%f" ,&c1[k].inserir[i]);
-			c1[k].vezes_inserir[i] = v;
-		}
 	}while (m!=2);
-	getch();
+		arq = fopen(Cpf, "wb");
+		fwrite(&c, sizeof(Cliente), 1, arq);
+		fclose (arq);
+	}
+	else 
+	printf ("\nSenha incorreta!!\n");
 }
+void retirar (char Cpf[]) {
+	char senha[4];
+	float retirar;
+	FILE *arq;
+	int i=0, k, m, v=0, t=1, opt;
+	Cliente c;
+	if (Cpf[0] == '\0') {
+	printf ("\nDigite seu CPF: ");
+	scanf ("%s" ,Cpf);
+	}
+	arq = fopen(Cpf, "rb");
+	if (arq == NULL) {
+	printf ("\nUsuario nao cadastrado!!\n");
+	return;
+	}
+	fread(&c, sizeof(Cliente), 1, arq);
+	fclose (arq);
+	printf ("\nDigite sua senha: ");
+	senha[0] = getch(); 
+	printf ("*"); 
+	senha[1] = getch(); 
+	printf ("*"); 
+	senha[2] = getch(); 
+	printf ("*"); 
+	senha[3] = getch();  
+	printf ("*\n"); 
+	if((senha[0] == c.senha[0]) && (senha[1] == c.senha[1]) && (senha[2] == c.senha[2]) && (senha[3] == c.senha[3]))  {
+	do {
+	printf ("\nRetira o saldo: ");
+	scanf ("%f",&retirar);
+		for (i=0;i<c.tam_retirou;i++) {
+			if (retirar == c.retirar[i]) {
+			c.vezes_r[i] ++;
+			v = 10;
+			}
+		}
+			if (v != 10){
+				k = c.tam_retirou;
+				c.retirar[k] = retirar;
+				c.tam_retirou ++;
+				c.vezes_r[k]=1;
+				v = 0;
+			}
+		c.saldo = c.saldo - retirar;
+		printf ("\nDeseja realizar outra operacao? Digite 1 para sim ou 2 para nao: ");
+		scanf ("%d" ,&m);
+	}while (m!=2);
+		arq = fopen(Cpf, "wb");
+		fwrite(&c, sizeof(Cliente), 1, arq);
+		fclose (arq);
+	}
+	else 
+	printf ("\nSenha incorreta!!\n");
+	
+}
+void extrato (char Cpf[]) {
+	char senha[4];
+	FILE *arq;
+	int i=0, k, m, v=1, t=1, o;
+	Cliente c;
+	if (Cpf[0] == '\0') {
+	printf ("\nDigite seu CPF: ");
+	scanf ("%s" ,Cpf);
+	}
+	arq = fopen(Cpf, "rb");
+	if (arq == NULL) {
+	printf ("\nUsuario nao cadastrado!!\n");
+	return;
+	}
+	fread(&c, sizeof(Cliente), 1, arq);
+	fclose (arq);
+	printf ("\nDigite sua senha: ");
+	senha[0] = getch(); 
+	printf ("*"); 
+	senha[1] = getch(); 
+	printf ("*"); 
+	senha[2] = getch(); 
+	printf ("*"); 
+	senha[3] = getch();  
+	printf ("*\n"); 
+	if((senha[0] == c.senha[0]) && (senha[1] == c.senha[1]) && (senha[2] == c.senha[2]) && (senha[3] == c.senha[3]))  {
+	printf ("\n\nInseriu:\tVezes: \n");
+	for (i=0;i<c.tam_inseriu;i++) {
+		printf ("R$ %.2f\t%d\n" ,c.inserir[i] ,c.vezes_inserir[i]);
+	}
+	printf ("\nretirou:\tVezes: \n");
+	for (i=0;i<c.tam_retirou;i++) {
+		printf ("R$ %.2f\t%d\n" ,c.retirar[i] ,c.vezes_r[i]);
+	}
+	printf ("\nSaldo: R$ %.2f\n" ,c.saldo);
+	}
+	else 
+	printf ("\nSenha incorreta!!\n");
+}
+void remover () {
+	char Cpf[10], senha[4];
+	FILE *arq;
+	Cliente c;
+	printf ("\nDigite seu CPF: ");
+	scanf ("%s" ,Cpf);
+	arq = fopen(Cpf, "rb");
+	if (arq == NULL) {
+	printf ("\nUsuario nao cadastrado!!\n");
+	return;
+	}
+	fread(&c, sizeof(Cliente), 1, arq);
+	fclose (arq);
+	printf ("\nDigite sua senha: ");
+	senha[0] = getch(); 
+	printf ("*"); 
+	senha[1] = getch(); 
+	printf ("*"); 
+	senha[2] = getch(); 
+	printf ("*"); 
+	senha[3] = getch();  
+	printf ("*\n"); 
+	if((senha[0] == c.senha[0]) && (senha[1] == c.senha[1]) && (senha[2] == c.senha[2]) && (senha[3] == c.senha[3]))  {
+	remove (Cpf);
+	}
+	else 
+	printf ("\nSenha incorreta!!\n");
+}
+#endif
--- aula20171207/minibanc.c
+++ aula20171207/minibanc.c
@@ -1,20 +1,37 @@
 #include<stdio.h>
 #include"funcoes.h"
 int main () {
+	char cpf[10];
 	int opt;
-	printf ("1 - Criar conta.\n2 - Mostrar conta.\n3 - Mostrar saldo.\n4 - Inserir dinheiro.\n5 - Retirar dinheiro.\n6 - Extrato.\n");
-	printf("7 - Remover conta.\n8 - Sair\nDigite:");
-	scanf ("%d" ,&opt);
+	printf ("Selecione: ");
 	do {
+	printf ("\n1 - Criar conta.\n2 - Mostrar conta.\n3 - Mostrar saldo.\n4 - Inserir dinheiro.\n5 - Retirar dinheiro.\n6 - Extrato.\n");
+	printf("7 - Remover conta.\n8 - Sair\n\nDigite: ");
+	scanf ("%d%*c" ,&opt);
 	switch(opt) {
 		case 1:
+			criar_conta();
+			break;
 		case 2:
+			mostrar_conta(cpf);
+			break;
 		case 3:
+			mostrar_saldo(cpf);
+			break;
 		case 4:
+			inserir_dinheiro(cpf);
+			break;
 		case 5:
+			retirar (cpf);
+			break;
 		case 6:
+			extrato (cpf);
+			break;
 		case 7:
-		default:	
+			remover ();
+			break;
+		default:
+		exit(0);	
 	}
 	}while(opt != 8);
 }
